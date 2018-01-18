bd63f17eca0b328de98de93d052703f7e0b80b04
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-18 11:22:12 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,6 +9,7 @@ class EmailParser
     individual_emails = @emails.split(/[, ]/).reject{ |x| x == ""}
     if individual_emails[0] == individual_emails[1] 
       individual_emails.shift 
+      individual_emails
     else
       individual_emails
     end
