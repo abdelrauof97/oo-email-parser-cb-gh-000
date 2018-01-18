ef91707adafbf14d527fc19c6861685762c34515
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-18 10:58:50 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,10 +1,14 @@
 
 class EmailParser  
   
-  attr_accessor :emails
+  attr_reader :emails
   
   def initialize(emails)
     @emails = emails
+  end
+  
+  def parse
+    @emails.split(/[, ]/).reject{ |x| x == ""}
   end
   
 end
